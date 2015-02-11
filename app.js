506ca3d240272c4a6348533836258abc81679515
gianordoli <gianordoligabriel@yahoo.com.br>
2015-02-11 00:41:28 -0500

    Loading languages and services from file

--- app.js
+++ app.js
@@ -1,112 +1,67 @@
 var request = require('request'),
-	 xml2js = require('xml2js');
+	 xml2js = require('xml2js'),
+		 jf = require('jsonfile'),
+	   util = require('util');
 
 var parser = new xml2js.Parser();
 
-var services = {
-youtube: { client: 'youtube', ds: 'yt', address: 'https://www.youtube.com/results?search_query=X' },
-books: { client: 'books', ds: 'bo', address: 'https://www.google.com/search?q=X&tbm=bks' },
-products: { client: 'products-cc', ds: 'sh', address: 'https://www.google.com/#q=X&tbm=shop' },
-news: { client: 'news-cc', ds: 'n', address: 'https://www.google.com/#q=X&tbm=nws' },
-images: { client: 'img', ds: 'i', address: 'https://www.google.com/search?site=imghp&tbm=isch&q=X&' },
-web: { client: 'psy', ds: '', address: 'https://www.google.com/#q=X' },
-recipes: { client: 'psy', ds: 'r', address: 'https://www.google.com/search?q=X&tbs=rcp'  }
-};
+var languages = jf.readFileSync('data/languages.json');
+// console.log(languages);
 
-var value = 'a';	
+var services = jf.readFileSync('data/services.json');
+// console.log(services);
 
-var service = services['images'];
+callAutocomplete('a', 'images', 'fr');
 
-// // Creating an api
-// var http = require('http');
-
-// var server = http.createServer(function(req, res) {
-// 	if(req.url === '/api') {
-// 		console.log('Called...');
-
-// 		request(
-// 			{	url: 'https://clients1.google.com/complete/search',
-// 			    dataType: 'jsonp',
-// 			    data: {
-// 					q: value,
-// 					nolabels: 't',
-// 					client: service.client,
-// 					ds: service.ds
-// 			    }
-// 			},
-// 			function(request, response, body){
-// 				console.log(JSON.stringify(response));
-// 			}
-// 		);
-
-// 	}
-// });
-
-// server.listen(3000, function() {
-// 	console.log('--> server listening to port: ' + 3000);
-// });
-
-// var labels = ['web', 'youtube', 'recipes', 'products', 'news', 'images', 'books'];
-
-// var opts = $.extend({service: 'web', secure: false}, opts);
-
-// var value = $(this).val();
-// var value = 'a';
-// console.log(value);
-
-// var service = services[labels[0]];
-
-// var url = 'https://clients1.google.com/complete/search?';
-// var options = [
-// 	{ param: 'q', value: 'a'},
-// 	{ param: 'client', value: 'toolbar'},
-// 	{ param: 'ds', value: ''}
-// ];
-
-// var services = {
-// 	images: {
-// 		url: 'https://www.google.com/search?site=imghp&tbm=isch',
-// 		options:
-// 		[	{ param: 'client', value: 'toolbar'},
-// 			{ param: 'ds', value: 'i'}	]
-// 	}
-// }
-// console.log(services.images.options);
-// var requestUrl = url;
-
-// console.log(requestUrl);
-
-function autocomplete(query, site){
+function concatenateUrl(query, site, language){
 	// console.log('q: ' + query + ', site: ' + site);
 	var service = services[site];
 	// console.log(service);	
 	var requestUrl = 'https://clients1.google.com/complete/search?' +
 					 '&client=toolbar'+
 					 '&q=' + query +
+					 '&hl=' + language +
 					 '&ds=' + service.ds;
 
 	// console.log(requestUrl);
 	return requestUrl;
 }
 
+function callAutocomplete(query, site, language){
+	
+	var url = concatenateUrl(query, site, language);
 
+	request(url, function (error, response, body) {
+		// console.log(error);
+		// console.log(response);
+		// console.log(body);
+		if (!error && response.statusCode == 200) {
 
-// request(autocomplete('a', 'images'), function (error, response, body) {
-	// console.log(error);
-	// console.log(response);
-	// console.log(body);
-// 	if (!error && response.statusCode == 200) {
+			var data;
+			parser.parseString(body, function (err, result) {
+				data = result.toplevel.CompleteSuggestion;
+			});
+			// console.log(data);
+			var suggestions = [];
+			data.forEach(function(element, index, array){
+				// console.log(element.suggestion[0]['$']['data']);
+				suggestions.push(element.suggestion[0]['$']['data']);
+			});		
+			// console.log(suggestions);
 
-// 		var data;
-// 		parser.parseString(body, function (err, result) {
-// 			data = result.toplevel.CompleteSuggestion;
-// 		});
-// 		// console.log(data);
-// 		var suggestions = [];
-// 		data.forEach(function(element, index, array){
-// 			// console.log(element.suggestion[0]['$']['data']);
-// 			suggestions.push(element.suggestion[0]['$']['data']);
-// 		});		
-// 		console.log(suggestions);
-// 	}
-// });
+			var newRecord = createRecord(site, language, suggestions);
+			// console.log(newRecord);
+		}
+	});
+}
+
+function createRecord(site, language, suggestions){
+	var date = new Date();
+	var obj = {
+		date: date.getTime(),
+		service: site,
+		language: language,
+		results: suggestions
+	};
+	return obj;
+}
