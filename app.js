3951d84618a830d251dc12fbb8dd369907544773
gianordoli <gianordoligabriel@yahoo.com.br>
2015-02-13 12:41:40 -0500

    JSON call + fixed encoding

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var request = require('request'),
-	 xml2js = require('xml2js'),
 		 jf = require('jsonfile'),
-	   util = require('util');
-
-var parser = new xml2js.Parser();
+	   util = require('util'),
+	  iconv = require('iconv-lite');
 
 var languages = jf.readFileSync('data/languages.json');
 // console.log(languages);
@@ -12,37 +10,40 @@ var services = jf.readFileSync('data/services.json');
 // console.log(services);
 
 var firstLetter = String.fromCharCode(65);
+// var site = 
 
 var dailySearch = [];
 
-callAutocomplete(firstLetter, 'images', 'fr');
+callAutocomplete(firstLetter, services[1], 'fr');
 
 
 /*-------------------- FUNCTIONS --------------------*/
 
-function callAutocomplete(query, site, language){
+function callAutocomplete(query, service, language){
 	
-	var url = concatenateUrl(query, site, language);
+	console.log(concatenateUrl(query, service, language));
+
+	var url = {
+		uri: concatenateUrl(query, service, language),
+		encoding: null
+	};
 
 	request(url, function (error, response, body) {
 		// console.log(error);
 		// console.log(response);
-		console.log(body);
+		// console.log(body);
+
 		if (!error && response.statusCode == 200) {
 
-			var data;
-			parser.parseString(body, function (err, result) {
-				data = result.toplevel.CompleteSuggestion;
-			});
+			var charset = getCharset(response);
+			// console.log(charset);
+
+			var data = JSON.parse(iconv.decode(body, 'ISO-8859-1'));
 			// console.log(data);
-			var suggestions = [];
-			data.forEach(function(element, index, array){
-				// console.log(element.suggestion[0]['$']['data']);
-				suggestions.push(element.suggestion[0]['$']['data']);
-			});		
+			var suggestions = data[1];
 			// console.log(suggestions);
 
-			var newRecord = createRecord(site, language, suggestions);
+			var newRecord = createRecord(service, language, suggestions);
 			console.log(newRecord);
 
 			// Save current result
@@ -53,13 +54,22 @@ function callAutocomplete(query, site, language){
 			// console.log(index);
 			index++;
 			if(index < 91){			
-				loopThroughLetters(index, site, language);
+					var newQuery = String.fromCharCode(index);
+					callAutocomplete(newQuery, service, language);
 			}else{
 				// console.log(dailySearch);
 				saveToDB();
 			}
 		}
 	});
+}
+
+function getCharset(response){
+	var headers = response.headers;
+	var contentType = headers['content-type'];
+	var charset = contentType.substring(contentType.lastIndexOf('=') + 1);
+	// console.log(charset);
+	return charset;
 }
 
 function saveToDB(){
@@ -75,17 +85,10 @@ function saveToDB(){
 	});
 }
 
-function loopThroughLetters(index, site, language){
-	var newQuery = String.fromCharCode(index);
-	callAutocomplete(newQuery, site, language);
-}
-
-function concatenateUrl(query, site, language){
-	// console.log('q: ' + query + ', site: ' + site);
-	var service = services[site];
-	// console.log(service);	
+function concatenateUrl(query, service, language){
+	// console.log(service.ds);	
 	var requestUrl = 'https://clients1.google.com/complete/search?' +
-					 '&client=toolbar'+
+					 '&client=firefox'+
 					 '&q=' + query +
 					 '&hl=' + language +
 					 '&ds=' + service.ds;
@@ -94,10 +97,10 @@ function concatenateUrl(query, site, language){
 	return requestUrl;
 }
 
-function createRecord(site, language, suggestions){
+function createRecord(service, language, suggestions){
 	var obj = {
 		date: new Date(),
-		service: site,
+		site: service.site,
 		language: language,
 		letter: suggestions[0].charAt(0),
 		results: suggestions
